44c5c209b332caf6680216a8ad22f91c85010895
Kawamura <=2026025@fsc-educ.com>
2026-06-12 11:39:00 +0900

    仮完成

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -1,14 +1,22 @@
 package com.example.blog_app;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class BlogController {
-    
-    @GetMapping("/blogs")
+     @GetMapping("/blogs")
     public String blogs() {
+
         return "/blogs";
+    }
+
+    @GetMapping("/blogs/new")
+    public String newBlog() {
+        return "/blogs/new";
     }
     
 }
